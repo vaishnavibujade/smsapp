8425ebc30d8e8f1381359af182d0ee5cb8b01ec0
DELL <DELL@DESKTOP-DT7V3KG>
2024-10-24 18:14:07 +0530

    add project

---
+++ smsapp/src/main/java/com/cjc/smsapp/app/model/Student.java
@@ -0,0 +1,74 @@
+package com.cjc.smsapp.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	private int studentId;
+	private String studentFullName;
+	private String studentEmail;
+	private int studentAge;
+	private String studentCollageName;
+	private String studentCourse;
+	private String batchNumber;
+	private String batchMode;
+	private double feespaid;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentFullName() {
+		return studentFullName;
+	}
+	public void setStudentFullName(String studentFullName) {
+		this.studentFullName = studentFullName;
+	}
+	public String getStudentEmail() {
+		return studentEmail;
+	}
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	public int getStudentAge() {
+		return studentAge;
+	}
+	public void setStudentAge(int studentAge) {
+		this.studentAge = studentAge;
+	}
+	public String getStudentCollageName() {
+		return studentCollageName;
+	}
+	public void setStudentCollageName(String studentCollageName) {
+		this.studentCollageName = studentCollageName;
+	}
+	public String getStudentCourse() {
+		return studentCourse;
+	}
+	public void setStudentCourse(String studentCourse) {
+		this.studentCourse = studentCourse;
+	}
+	public String getBatchNumber() {
+		return batchNumber;
+	}
+	public void setBatchNumber(String batchNumber) {
+		this.batchNumber = batchNumber;
+	}
+	public String getBatchMode() {
+		return batchMode;
+	}
+	public void setBatchMode(String batchMode) {
+		this.batchMode = batchMode;
+	}
+	public double getFeespaid() {
+		return feespaid;
+	}
+	public void setFeespaid(double feespaid) {
+		this.feespaid = feespaid;
+	}
+	
+
+}
---
+++ smsapp/src/test/java/com/cjc/smsapp/SmsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.smsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
